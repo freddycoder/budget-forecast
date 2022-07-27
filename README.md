ec38f33f28bce67eccc4e1683895fdcc7b6c8cfe
Frédéric Jacques <gary.bob88@gmail.com>
2022-07-26 23:44:09 -0400

    add basename in the url when deploy on github

--- README.md
+++ README.md
@@ -6,5 +6,13 @@ I little app I build to help me forecast my budget and learn React.
 
 - The app work in french or english
 - Ability to enter mortgage
-- Ability to enter income and expenses (work in progress)
-- Print a simulation of the budget (work in progress)
+- Ability to enter income and expenses
+- Print a simulation of the budget
+
+### Deploy as github pages
+
+```
+npm run deploy
+```
+
+source: https://reactjsexample.com/a-guide-for-hosting-a-react-app-on-github-pages/
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
 import { Simulation } from './features/mortgage/Simulation';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Income } from './features/income/Income';
 import { Outcome } from './features/outcome/Outcome';
 import { RenderSimulation } from './features/finalSimulation/RenderSimulation';
 import { NavBar } from './NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar></NavBar>
-      </header>
+  let baseName = undefined;
 
-      <main className="App-main">
-        <article>
-          <section>
-            <Router>
-              <Routes>
-                <Route path="/" element={<Simulation></Simulation>} />
-                <Route path="/hypotheque" element={<Simulation></Simulation>} />
-                <Route path="/revenue" element={<Income></Income>} />
-                <Route path="/depense" element={<Outcome></Outcome>} />
-                <Route path="/simulation" element={<RenderSimulation></RenderSimulation>} />
-              </Routes>
-            </Router>
-          </section>
-        </article>
-      </main>
-    </div>
+  if (location.host.includes('github.io')) {
+    baseName = '/buget-forecast';
+  }
+
+  return (
+    <BrowserRouter basename={baseName}>
+      <div className="App">
+        <header className="App-header">
+          <NavBar></NavBar>
+        </header>
+
+        <main className="App-main">
+          <article>
+            <section>
+              <Router>
+                <Routes>
+                  <Route path="/" element={<Simulation></Simulation>} />
+                  <Route path="/hypotheque" element={<Simulation></Simulation>} />
+                  <Route path="/revenue" element={<Income></Income>} />
+                  <Route path="/depense" element={<Outcome></Outcome>} />
+                  <Route path="/simulation" element={<RenderSimulation></RenderSimulation>} />
+                </Routes>
+              </Router>
+            </section>
+          </article>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
