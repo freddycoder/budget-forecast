37618479a8821ae035cee34abbb67d5839891569
Frédéric Jacques <gary.bob88@gmail.com>
2022-09-02 13:55:14 -0400

    add null as parameter

--- src/utils/formatUtil.ts
+++ src/utils/formatUtil.ts
@@ -1,4 +1,4 @@
-export const format = (num: number) => {
+export const format = (num: number | null) => {
     if (num != null) {
         return num.toLocaleString('fr-CA', {
             minimumFractionDigits: 2,
